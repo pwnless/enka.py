d844e8af13b1b31d9adf93b5db7fe4e169afe499
pwnblog <99332743+pwnblog@users.noreply.github.com>
2022-06-29 22:01:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     description="Library for fetching JSON data from site https://enka.shinshin.moe/",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/mrwan200/enkanetwork-py",
+    url="https://github.com/pwnblog/enka.py",
     keywords=['enkapy.py', 'enkapy', 'enka.shinshin.moe', 'genshinapi'],
     packages=setuptools.find_packages(),
     classifiers=[
