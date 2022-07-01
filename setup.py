1b33b3feba610e892f3783a0ccaf9592d3f728f2
pwnblog <99332743+pwnblog@users.noreply.github.com>
2022-07-01 16:17:01 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="enka.py",
-    version="1.0.2",
+    version="1.0.6",
     author="pwnblog",
     description="Library for fetching JSON data from site https://enka.shinshin.moe/",
     long_description=open("README.md", "r", encoding="utf-8").read(),
