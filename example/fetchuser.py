f144731e185dd7458954d596886026a786ef418b
pwnblog <99332743+pwnblog@users.noreply.github.com>
2022-07-31 15:22:55 +0800

    Update fetchuser.py

--- example/fetchuser.py
+++ example/fetchuser.py
@@ -4,21 +4,39 @@ from enkapy import Enka
 
 client = Enka()
 
-
 async def main():
     await client.load_lang()
     user = await client.fetch_user(104267816)
     print(f"Nickname: {user.player.nickname}")
     print(f"Level: {user.player.level}")
-    for character in user.characters:
-        print(f'Name: {character.name}')
-        print(f'Weapon: {character.weapon.nameText}')
-        print('Artifacts:')
-        for artifact in character.artifacts:
-            print(f'\t{artifact.setNameText} {artifact.nameText}:')
-            print(f'\t{artifact.main_stat.prop}:{artifact.main_stat.value}')
-            for sub_stats in artifact.sub_stats:
-                print(f'\t\t{sub_stats.prop}:{sub_stats.value}')
+    # fetch first character
+    character = user.characters[0]
+    print(f'Name: {character.name}')
+    print('Weapon:')
+    weapon = character.weapon
+    print(f'\tName: {weapon.name}')
+    print(f'\tLevel: {weapon.level}')
+    print(f'\tRefine: {weapon.refine}')
+    print(f'\tStar level: {weapon.rank}')
+
+    print('Constellations:')
+    for constellation in character.constellations:
+        if constellation.activated:
+            print(f'\t{constellation.name} Activated')
+    print('Skills:')
+    for skill in character.skills:
+        if skill.type == 0:
+            print(f'\tNormal skill {skill.name}, level:{skill.level}')
+        elif skill.type == 1:
+            print(f'\tElemental skill {skill.name}, level:{skill.level}')
+        elif skill.type == 2:
+            print(f'\tElemental burst {skill.name}, level:{skill.level}')
+    print('Artifacts:')
+    for artifact in character.artifacts:
+        print(f'\t{artifact.setNameText} {artifact.nameText}:')
+        print(f'\t{artifact.main_stat.prop}:{artifact.main_stat.value}')
+        for sub_stats in artifact.sub_stats:
+            print(f'\t\t{sub_stats.prop}:{sub_stats.value}')
 
 
 loop = asyncio.get_event_loop()
