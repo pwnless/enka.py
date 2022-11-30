452165d30af86600cc1d52b5da29821ccc42db2b
pwnblog <99332743+pwnblog@users.noreply.github.com>
2022-11-30 22:36:00 +0800

    Update version 12

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, pwnblog'
 author = 'pwnblog'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.10'
+release = '1.0.12'
 
 
 # -- General configuration ---------------------------------------------------
