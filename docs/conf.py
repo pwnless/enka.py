4d4fac3bf79ba72f180b47cdb34a316481a81d8a
pwnblog <99332743+pwnblog@users.noreply.github.com>
2023-01-16 19:30:50 +0800

    Update 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, pwnblog'
 author = 'pwnblog'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
