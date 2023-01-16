351db29945c7f0d8feb2bdd96617e7525205637f
pwnblog <99332743+pwnblog@users.noreply.github.com>
2023-01-16 19:30:30 +0800

    Update 1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="enka.py",
-    version="1.1.0",
+    version="1.1.1",
     author="pwnblog",
     description="Library for fetching JSON data from site https://enka.network/",
     long_description=open("README.md", "r", encoding="utf-8").read(),
