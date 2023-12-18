1446dc960be6ddf74a36f839e668569f93b0b470
hopeless <99332743+pwnless@users.noreply.github.com>
2023-12-18 11:31:51 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, pwnless'
 author = 'pwnless'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.5'
+release = '1.1.8'
 
 
 # -- General configuration ---------------------------------------------------
